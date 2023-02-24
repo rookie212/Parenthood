bc925b80b7bb06a45320ec41de6bf7f6ad6be68c
HarmanSMann <76751076+HarmanSMann@users.noreply.github.com>
2023-02-23 21:40:46 -0500

    push for testing

--- app/src/main/java/com/humber/parenthood/MainActivity.java
+++ app/src/main/java/com/humber/parenthood/MainActivity.java
@@ -4,17 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.Button;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.humber.parenthood.databinding.ActivityMainBinding;
+import com.humber.parenthood.eat_in_layout.IngredientPicker;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
